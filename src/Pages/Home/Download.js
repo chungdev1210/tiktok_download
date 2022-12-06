7ca999321ddde577aad010bd1016db40557b6140
Dai Nguyen <dai@devnguyen.com>
2022-12-06 15:22:23 +0700

    file save

--- src/Pages/Home/Download.js
+++ src/Pages/Home/Download.js
@@ -2,11 +2,21 @@ import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { StateContext } from '../../Services/Context/StateProvider';
 import axios from 'axios';
+import { saveAs } from 'file-saver';
 
 export default function Download() {
    const { dataTiktok, resetData } =  useContext(StateContext)
    const { data } = dataTiktok;
-   console.log(data.video_data)
+
+   const handleDownload = (e) => {
+      e.preventDefault();
+
+      let _file_name     = data.desc.toString().replace(/[^a-zA-Z0-9 ]/g, '_');
+      let _file_type     = 'mp4';
+      let _link_download = data.video_data.nwm_video_url_HQ;
+
+      saveAs(_link_download, _file_name + '.' + _file_type);
+   }
    
    return (
       <div className='download__video mt-2'>
@@ -22,10 +32,10 @@ export default function Download() {
                   </div>
                </div>
                <div className='col-12 col-md-4 offset-md-2 down__right'>
-                  <a href="#" className='down__server1 btn btn-primary mb-2' download={data.video_data.nwm_video_url}>
+                  <a href="#" className='down__server1 btn btn-primary mb-2' onClick={handleDownload}>
                      Download server 1
                   </a>
-                  <a href="#" className='down__server2 btn btn-success mb-2' download={data.video_data.nwm_video_url_HQ}>Download server 2</a>
+                  <a href="#" className='down__server2 btn btn-success mb-2' onClick={handleDownload}>Download server 2</a>
                </div>
             </div>
             <hr className='mt-5'/>
