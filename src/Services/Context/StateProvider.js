cdac6f886c2f265c7ea5a8874a1e50d161edaeb6
Chung Nguyen <nvchung.zmg@gmail.com>
2022-12-24 16:04:50 +0700

    edit ip

--- src/Services/Context/StateProvider.js
+++ src/Services/Context/StateProvider.js
@@ -27,9 +27,12 @@ export default function StateProvider({ children }) {
    const getData = (link) => {
       handleLoading()
       const linkData = SERVER_API
-      let ipUser = localStorage.getItem("ip");
-      ipUser = JSON.parse(ipUser);
-      const { ip } = ipUser;
+      let ip = "";
+      if (localStorage.getItem("ip")) {
+         let ipUser = localStorage.getItem("ip");
+         ipUser = JSON.parse(ipUser);
+         ip = ipUser.ip
+      }
       axios.post(linkData, {
          query: link,
          ip: ip,
